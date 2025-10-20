8c5eebc69b99902ab92387c9302c6fc5afef2f70
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2025-10-20 12:13:00 +0200

    reset webpage

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,15 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config'
+import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
-import db from '@astrojs/db'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   adapter: vercel(),
-  integrations: [sitemap(), db(), icon()],
-  experimental: {
-    fonts: [
-      {
-        cssVariable: '--font-sofia-sans',
-        name: 'Sofia Sans',
-        provider: fontProviders.google()
-      }
-    ]
-  },
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()]
   }
--- db/seed.ts
+++
@@ -1,9 +0,0 @@
-import { db, Guestbook } from 'astro:db'
-
-export default async function () {
-  await db.insert(Guestbook).values([
-    { id: 1, name: 'Eva', text: 'Hello, world!' },
-    { id: 2, name: 'Paul', text: 'Hello, world!' },
-    { id: 3, name: 'Waxer', text: 'Hello, world!' }
-  ])
-}
